7285f0ac8e10695318755c644e447c9db72c58e9
Henrique Barbosa <henrikleod@gmail.com>
2022-02-06 15:34:07 -0300

    Getting json with sandbox variables

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from collector.coinmarketcap import get_cryptocurrencies
+from functools import lru_cache
+from config import Settings
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@lru_cache()
+def get_settings():
+    return Settings()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(settings: Settings = Depends(get_settings)):
+    return get_cryptocurrencies(
+        {
+            "URL": settings.coinmarketcap_apiurl,
+            "APIKEY": settings.coinmarketcap_apikey
+        }
+    )
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
